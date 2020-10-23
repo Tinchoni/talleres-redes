48eeed8f7be4eebe1c37a87dd497c8fa57b0488f
Martin <38443874+Tinchoni@users.noreply.github.com>
2020-10-23 16:25:44 -0300

    ya sabes feli vacunate

--- taller3/taller3.py
+++ taller3/taller3.py
@@ -16,17 +16,19 @@ def write_to_csv(line):
 write_to_csv("port,protocolo,estado,flags\n")
 
 for i in ports:
+
+	#TCP:
 	tcp_packet = IP(dst=ip)/TCP(dport=i, flags='S')
 	print(i, end='')
 	estado = ''
-	resp = sr1(tcp_packet, verbose=False, timeout=1.0) # "Send packets at layer 3 and return only the first answer". Meh, devuelve un paquete de nivel 3 obvio, despues lo vas a desmenuzar pa analizar. 
+	resp = sr1(tcp_packet, verbose=False, timeout=1.0)
 	if resp is None:
 		print(" filtrado")
 		estado = 'filtrado'
 		flags = ''
 	elif resp.haslayer(TCP):
 		tcp_layer = resp.getlayer(TCP)
-		if tcp_layer.flags == 0x12: # por eso en el interprete, te salta que tiene Flag 18 (SA)
+		if tcp_layer.flags == 0x12:
 			estado = 'abierto'
 			print(" abierto", tcp_layer.flags)
 			sr1(IP(dst=ip)/TCP(dport=ports, flags='AR'), verbose=False, timeout=1) 
@@ -36,7 +38,6 @@ for i in ports:
 		flags = tcp_layer.flags
 
 	write_to_csv(f'{i},tcp,{estado},{flags}\n')
-
 
 	#UDP:
 	udp_packet = IP(dst=ip)/UDP(dport=i)
@@ -55,15 +56,6 @@ for i in ports:
 
 	write_to_csv(f'{i},udp,{estado},\n')
 
-
-# y ahora pa escanear paquetes UDP? acá nmap lo explica: https://nmap.org/book/scan-methods-udp-scan.html
-
-#	elif resp.haslayer(UDP): ... OOOO resp.haslayer(ICMP) y ver si el mensaje es Port Unreachable (o sea, ver resp.getlayer(UDP).type == 3 y code == 3)
-# podes chusmear con el interprete, hacer probe = blabla y probe.show().
-
-# queremos un csv con... port,protocolo,estado,flags
-
-
 #	https://scapy.readthedocs.io/en/latest/usage.html#send-and-receive-packets-sr
-#	 https://scapy.readthedocs.io/en/latest/usage.html#udp-ping
+#	https://scapy.readthedocs.io/en/latest/usage.html#udp-ping
 #	https://scapy.readthedocs.io/en/latest/usage.html#tcp-port-scanning
